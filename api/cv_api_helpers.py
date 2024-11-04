2ae7abd283ee931c534e5173c7c5c0e46a8a3a7b
kromsam <mail@samsimons.nl>
2024-11-04 21:35:51 +0100

    check for errors in API response

--- api/cv_api_helpers.py
+++ api/cv_api_helpers.py
@@ -21,6 +21,13 @@ def fetch_data_from_api(base_url, endpoint, params):
     if response:
         print("Request URL: %s", response.url)
         response_data = response.json()
+        
+        # Check if the first key of the dict is "error"
+        if list(response_data.keys())[0] == "error":
+            error_code = response_data["error"].get("code")
+            unique_identifier = response_data["error"].get("unique_identifier")
+            raise ValueError(f"Error {error_code}: {unique_identifier}")
+        
         count = response_data.get("count")
         total_count = response_data.get("totalCount")
     else:
@@ -41,6 +48,13 @@ def fetch_data_from_api(base_url, endpoint, params):
             response = get_api_response(base_url, endpoint, params)
             if response:
                 response_data = response.json()
+                
+                # Check if the first key of the dict is "error"
+                if list(response_data.keys())[0] == "error":
+                    error_code = response_data["error"].get("code")
+                    unique_identifier = response_data["error"].get("unique_identifier")
+                    raise ValueError(f"Error {error_code}: {unique_identifier}")
+                
                 response_block[response_endpoint].extend(
                     response_data["_embedded"][response_endpoint]
                 )
@@ -75,6 +89,7 @@ def get_api_response(base_url, endpoint, params):
     try:
         response = httpx.get(base_url + endpoint, params=params, timeout=30)
         response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
+        
         return response
     except httpx.HTTPError as http_err:
         logger.error("HTTP error occurred: %s", http_err)
