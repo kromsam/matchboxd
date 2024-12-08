4b692980531a92b5949ac48ec0f761af9f2a7025
kromsam <mail@samsimons.nl>
2024-12-08 23:57:42 +0100

    Refactor entrypoint.sh to add PYTHONPATH to environment variables and simplify script execution

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# Add Python path to environment variables
+export PYTHONPATH="/usr/src/app:$PYTHONPATH"
+
 # Function to run the Python script and check its exit status
 run_script() {
-    python -m /usr/src/app/matchboxd_scraper # Use absolute path to Python
+    python -m matchboxd_scraper # Use absolute path to Python
     return $?                   # Return the exit status of the script
 }
 
@@ -25,9 +28,6 @@ echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >/etc/c
 
 # Export environment variables for cron job
 printenv | grep -v "no_proxy" >>/etc/cron.d/matchboxd
-
-# Add Python path to environment variables
-export PYTHONPATH="/usr/src/app:$PYTHONPATH"
 
 # Add cron schedule for running Python script
 echo "$CRON_SCHEDULE python -m matchboxd_scraper >> /var/log/matchboxd.log 2>&1" >>/etc/cron.d/matchboxd
