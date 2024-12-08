4d4e04be1d8cc8e3d5daf10426808af1aea0f1f9
kromsam <mail@samsimons.nl>
2024-12-08 23:41:56 +0100

    Update entrypoint.sh to use relative Python path for script execution in cron job

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 # Function to run the Python script and check its exit status
 run_script() {
-    /usr/bin/python -m matchboxd_scraper # Use absolute path to Python
-    return $?                             # Return the exit status of the script
+    python -m matchboxd_scraper # Use absolute path to Python
+    return $?                   # Return the exit status of the script
 }
 
 # Check if RUN_ONCE is set to true
@@ -27,7 +27,7 @@ echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >/etc/c
 printenv | grep -v "no_proxy" >>/etc/cron.d/matchboxd
 
 # Add cron schedule for running Python script
-echo "$CRON_SCHEDULE /usr/bin/python -m matchboxd_scraper >> /var/log/matchboxd.log 2>&1" >>/etc/cron.d/matchboxd
+echo "$CRON_SCHEDULE python -m matchboxd_scraper >> /var/log/matchboxd.log 2>&1" >>/etc/cron.d/matchboxd
 
 # Apply permissions and load cron job
 chmod 0644 /etc/cron.d/matchboxd
