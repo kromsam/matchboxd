da362b15bd2f924341dd976923dac010a92f6f15
kromsam <mail@samsimons.nl>
2024-12-10 19:29:16 +0100

    update Dockerfile and docker-compose for improved configuration and add config.toml to .gitignore

--- api/main.py
+++ api/main.py
@@ -5,9 +5,9 @@ import logging
 from fastapi import FastAPI
 from fastapi_sqlalchemy import DBSessionMiddleware
 
-from .app import router
-from .config import LOG_LEVEL, LOG_FORMAT
-from .global_constants import DATABASE
+from api.app import router
+from api.config import LOG_LEVEL, LOG_FORMAT
+from api.global_constants import DATABASE
 
 
 # Set up logging
