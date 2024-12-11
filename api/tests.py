f3300d17f9c6360c9273502c61841fb891524037
kromsam <mail@samsimons.nl>
2024-12-11 16:04:41 +0100

    refactor tests.py to use logger instead of print statements and update import paths

--- api/tests.py
+++ api/tests.py
@@ -2,18 +2,18 @@
 
 import logging
 
-from .global_constants import COUNTRY, CITY, DATABASE
+from api.global_constants import COUNTRY, CITY, DATABASE
 
-from .db_helpers import DatabaseHandler
-from .cv_api_helpers import get_api_url
-from .cv_api_helpers import fetch_data_from_api
-from .cv_api_helpers import get_start_date
-from .cv_api import import_cities_to_db
-from .cv_api import import_events_to_db
-from .cv_api import import_productions_to_db
-from .cv_api import remove_films
-from .cv_api import remove_showings
-from .tmdb_api import update_tmdb_id, update_tmdb_image
+from api.db_helpers import DatabaseHandler
+from api.cv_api_helpers import get_api_url
+from api.cv_api_helpers import fetch_data_from_api
+from api.cv_api_helpers import get_start_date
+from api.cv_api import import_cities_to_db
+from api.cv_api import import_events_to_db
+from api.cv_api import import_productions_to_db
+from api.cv_api import remove_films
+from api.cv_api import remove_showings
+from api.tmdb_api import update_tmdb_id, update_tmdb_image
 
 # Import the root logger
 logger = logging.getLogger(__name__)
@@ -34,19 +34,19 @@ def import_cities(db_handler):
 
 def import_events(db_handler):
     """Import events into the database."""
-    print("Import events")
+    logger.info("Import events")
     session = db_handler.create_session()
     # clear_tables(session, Showing.__table__)
     city = CITY
-    print(city)
+    logger.info(city)
     country = COUNTRY
-    print(country)
+    logger.info(country)
     start_date = get_start_date(hours_delta=1)
-    print(start_date)
+    logger.info(start_date)
     base_url, endpoint, params = get_api_url(
         "events", country=country, city=city, start_date=start_date
     )
-    print("Fetch data from API with: ", base_url, endpoint, params)
+    logger.info("Fetch data from API with: ", base_url, endpoint, params)
     cv_api_data = fetch_data_from_api(base_url, endpoint, params)
     updated_cv_showings, updated_cv_film_titles = import_events_to_db(
         session, cv_api_data, city
@@ -62,12 +62,12 @@ def import_productions(db_handler):
     """Import productions into the database."""
     # Replace 'your_database_path' with the path to your SQLite database file
     session = db_handler.create_session()
-    print("Import productions...")
+    logger.info("Import productions...")
     city = CITY
     country = COUNTRY
-    print("Getting API url...")
+    logger.info("Getting API url...")
     base_url, endpoint, params = get_api_url("productions", country=country, city=city)
-    print("Fetch data from url...")
+    logger.info("Fetch data from url...")
     cv_api_data = fetch_data_from_api(base_url, endpoint, params)
     updated_cv_titles = import_productions_to_db(session, cv_api_data, city=city)
     # logger.info("Updated Titles: %s", updated_cv_titles)
