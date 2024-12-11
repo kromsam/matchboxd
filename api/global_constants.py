ec57e1349008814513b624623b44cabb4a2fd1ec
kromsam <mail@samsimons.nl>
2024-12-11 03:42:01 +0100

    refactor global constants to use get method for safer access to configuration values

--- api/global_constants.py
+++ api/global_constants.py
@@ -9,16 +9,16 @@ with open(config_path, "rb") as f:
     config = tomllib.load(f)
 
 # Settings
-COUNTRY = os.getenv("COUNTRY", config["location"]["country"])
-CITY = os.getenv("CITY", config["location"]["city"])
+COUNTRY = os.getenv("COUNTRY", config["location"].get("country", ""))
+CITY = os.getenv("CITY", config["location"].get("city", ""))
 
 # Database
-DB_TYPE = os.getenv("DB_TYPE", config["database"]["type"])
-DB_NAME = os.getenv("DB_NAME", config["database"]["name"])
-DB_USER = os.getenv("DB_USER", config["database"]["user"])
-DB_PASSWORD = os.getenv("DB_PASSWORD", config["database"]["password"])
-DB_HOST = os.getenv("DB_HOST", config["database"]["host"])
-DB_PORT = os.getenv("DB_PORT", config["database"]["port"])
+DB_TYPE = os.getenv("DB_TYPE", config["database"].get("type", ""))
+DB_NAME = os.getenv("DB_NAME", config["database"].get("name", ""))
+DB_USER = os.getenv("DB_USER", config["database"].get("user", ""))
+DB_PASSWORD = os.getenv("DB_PASSWORD", config["database"].get("password", ""))
+DB_HOST = os.getenv("DB_HOST", config["database"].get("host", ""))
+DB_PORT = os.getenv("DB_PORT", config["database"].get("port", ""))
 
 # Cineville API
 CV_API = {
@@ -27,15 +27,15 @@ CV_API = {
 }
 
 # Letterboxd List API
-LB_LIST_API = os.getenv("LB_LIST_API", config["lb_list_api"]["url"])
+LB_LIST_API = os.getenv("LB_LIST_API", config["lb_list_api"].get("url", ""))
 
 # The Movie Database API
-TMDB_API = os.getenv("TMDB_API", config["tmdb_api"]["url"])
-TMDB_API_KEY = os.getenv("TMDB_API_KEY", config["tmdb_api"]["key"])
+TMDB_API = os.getenv("TMDB_API", config["tmdb_api"].get("url", ""))
+TMDB_API_KEY = os.getenv("TMDB_API_KEY", config["tmdb_api"].get("key", ""))
 
 # Logging
-LOG_LEVEL = os.getenv("LOG_LEVEL", config["logging"]["level"])
-LOG_FORMAT = os.getenv("LOG_FORMAT", config["logging"]["format"])
+LOG_LEVEL = os.getenv("LOG_LEVEL", config["logging"].get("level", ""))
+LOG_FORMAT = os.getenv("LOG_FORMAT", config["logging"].get("format", ""))
 
 # Do not edit
 
